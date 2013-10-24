76613a2431b919e20881ed958490cec43fc746b0
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2013-10-24 18:03:55 +0300

    Revert "updated installer.sh"
    This reverts commit 33e1f882d60eafee694ebd18bc62380e361489f0.

--- src/main/scripts/installer.sh
+++ src/main/scripts/installer.sh
@@ -2,17 +2,15 @@
 # file used to automatically deploy the resizing scripts and the 
 # orchestrator war.
 
-source /etc/profile
-
-[ -z "$CATALINA_BASE" ] && echo "No CATALINA_BASE found, exiting.." && exit 1;
+[ -z "$CATALINA_BASE"] && echo "No CATALINA_BASE found, exiting.." && exit 1;
 
 function deploy_war {
-        WAR=`find . -name "*.war"`;
-        cp -v $WAR $CATALINA_BASE/webapps/;
-}
+	WAR=find . -name "*.war";
+	cp $WAR $CATALINA_BASE/webapps/;
+} 
 
 function deploy_scripts {
-        cp -v scripts/* /usr/local/bin/
+	cp scripts/* /usr/local/bin/
 }
 
 deploy_scripts
