0d80e81afc84d9ed15cf0d710605192791f0f4d9
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2014-09-24 15:14:19 +0100

    jersey support for REST API and first calls for DM

---
+++ war/src/main/java/gr/ntua/cslab/rest/ResizingAction.java
@@ -0,0 +1,40 @@
+
+package gr.ntua.cslab.rest;
+
+import gr.ntua.cslab.rest.beans.ResizingActionDetail;
+import java.util.LinkedList;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Resizing action resource API.
+ * @author Giannis Giannakopoulos
+ */
+@Path("/resizing/")
+public class ResizingAction {
+    
+    @GET
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public List<ResizingActionDetail> listResizingActions(){
+       List<ResizingActionDetail> ret = new LinkedList<>();
+       ret.add(new ResizingActionDetail(1, "add VM"));
+       ret.add(new ResizingActionDetail(2, "remove VM"));
+       return ret;
+    }
+    
+    @POST
+    public Response executeResizingAction(){
+       return Response.status(200).build();
+    }
+    
+    @GET
+    @Path("{id}/")
+    public Response getResizingActionStatus(){
+       return Response.status(200).build();
+    }
+}
---
+++ war/src/main/java/gr/ntua/cslab/rest/beans/ResizingActionDetail.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.ntua.cslab.rest.beans;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author giannis
+ */
+
+@XmlRootElement(name = "resizingAction")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ResizingActionDetail {
+    
+    private int id;
+    private String name;
+
+    public ResizingActionDetail() {
+    }
+
+    public ResizingActionDetail(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
