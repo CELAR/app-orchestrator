80ccf63ed9adbfde134c568114802df419765a7b
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2014-09-25 14:24:23 +0100

    resizing resource finalized

--- war/src/main/java/gr/ntua/cslab/orchestrator/rest/ResizingActionResource.java
+++ war/src/main/java/gr/ntua/cslab/orchestrator/rest/ResizingActionResource.java
@@ -1,4 +1,18 @@
-
+/*
+ * Copyright 2014 CELAR.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package gr.ntua.cslab.orchestrator.rest;
 
 import gr.ntua.cslab.orchestrator.beans.ExecutedResizingAction;
@@ -34,21 +48,15 @@ public class ResizingActionResource {
     
     @POST
     @Consumes(MediaType.APPLICATION_XML)
-    public ExecutedResizingAction executeResizingAction(@QueryParam("action_id") int actionId
-//            Parameters params
-    ){
+    public ExecutedResizingAction executeResizingAction(@QueryParam("action_id") int actionId, Parameters params){
         ExecutedResizingAction exec = new ExecutedResizingAction();
         exec.setExecutionStatus(ResizingExecutionStatus.ONGOING);
         exec.setResizingAction(ResizingActionsCache.getResizingActionById(actionId));
         exec.setUniqueId(UUID.randomUUID().toString());
+        exec.setParameters(params);
+        exec.setTimestamp(System.currentTimeMillis());
         
-        Parameters parameters = new Parameters();
-        parameters.addParameter(new Parameter("cpu", "2"));
-        parameters.addParameter(new Parameter("ram", "2"));
-        exec.setParameters(parameters);
-        ResizingActionsCache.addExecutedResizingAction(exec);
-        System.err.println("Wrote action to history");
-                
+        ResizingActionsCache.addExecutedResizingAction(exec);          
         return exec;
     }
     
