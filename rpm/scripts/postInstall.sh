934c6338ce1422ef5c78735c9cfa70ae7832376a
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2014-10-08 19:33:02 +0300

    configuration done

--- rpm/scripts/postInstall.sh
+++ rpm/scripts/postInstall.sh
@@ -3,7 +3,10 @@
 RANDOM_PASSWORD=$(/bin/cat /dev/urandom | tr -dc 0-9a-zA-Z | head -c 16)
 CELAR_ORCHESTRATOR_HOME=/opt/celar/celar-orchestrator
 KEYSTORE_PATH=$CELAR_ORCHESTRATOR_HOME/keystore.jks
-CONF_FILE=$CELAR_ORCHESTRATOR_HOME/conf/celar-orchestrator.properties
+CONF_FILE=$CELAR_ORCHESTRATOR_HOME/conf/orchestrator.properties
+
+
+SLIPSTREAM_CONTEXT=/tmp/slipstream.context
 
 create_keystore(){
 /usr/bin/keytool -genkey \
@@ -27,9 +30,12 @@ configure_server(){
 #/bin/sed -i 's|# server.ssl.keystore.path = |server.ssl.keystore.path = '$KEYSTORE_PATH'|' $CONF_FILE;
 #/bin/sed -i "s/# server.ssl.keystore.password = /server.ssl.keystore.password = $RANDOM_PASSWORD/" $CONF_FILE
 #/bin/sed -i "s/# server.ssl.port = 8443/server.ssl.port = 8443/" $CONF_FILE
-echo PLACEHOLDER
-echo "Puts some configuration code in here"
-exit 1
+
+DEPLOYMENT_ID=$(grep diid $SLIPSTREAM_CONTEXT | awk '{print $3}')
+SS_SERVER_IP=$(grep serviceurl $SLIPSTREAM_CONTEXT  |  tr '/' '\t' | awk '{print $4}')
+/bin/sed -i "s/# slipstream.deployment.id = /slipstream.deployment.id = $DEPLOYMENT_ID/" $CONF_FILE
+/bin/sed -i "s/# slipstream.server.host = /slipstream.server.host = $SS_SERVER_IP/" $CONF_FILE
+/bin/sed -i "s/# celar.server.host = /celar.server.host = $SS_SERVER_IP/" $CONF_FILE
 }
 
 create_service(){
