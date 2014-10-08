6cc8e7c3e357b97c6cafd17e8dde78d2e4fc7c43
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2014-10-08 18:08:39 +0300

    major changes

---
+++ orchestrator-daemon/src/main/java/gr/ntua/cslab/orchestrator/shared/ServerStaticComponents.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.ntua.cslab.orchestrator.shared;
+
+import java.util.Properties;
+import org.eclipse.jetty.server.Server;
+
+/**
+ *
+ * @author giannis
+ */
+public class ServerStaticComponents {
+    public static Properties properties;
+    public static Server server;
+    
+}
