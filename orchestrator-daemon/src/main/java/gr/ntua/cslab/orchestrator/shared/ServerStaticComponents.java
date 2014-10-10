16518d8c31c92b8cc1a13a3e51fc97e2b245dc98
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2014-10-10 12:08:18 +0300

    stub method for the configuration of the orchestrator

--- orchestrator-daemon/src/main/java/gr/ntua/cslab/orchestrator/shared/ServerStaticComponents.java
+++ orchestrator-daemon/src/main/java/gr/ntua/cslab/orchestrator/shared/ServerStaticComponents.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2014 CELAR.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package gr.ntua.cslab.orchestrator.shared;
 
@@ -10,10 +20,17 @@ import org.eclipse.jetty.server.Server;
 
 /**
  *
- * @author giannis
+ * @author Giannis Giannakopoulos
  */
 public class ServerStaticComponents {
+    /**
+     * Properties object, loaded during the bootstrapping of the daemon.
+     */
     public static Properties properties;
+    
+    /**
+     * Server object.
+     */
     public static Server server;
     
 }
