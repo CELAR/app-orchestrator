04b644ef5ab84d2c5146fb1d8204545f2775395e
Giannis Giannakopoulos <ggian@cslab.ece.ntua.gr>
2014-10-14 14:15:07 +0300

    minor fixes

--- rpm/scripts/preRemove.sh
+++ rpm/scripts/preRemove.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 service celar-orchestrator stop
+kill -9 $(cat /tmp/celar-orchestrator.pid)
 rm  -rf /etc/init.d/celar-orchestrator
 rm  -rf /opt/celar/celar-orchestrator/keystore.jks
 rm  -rf /tmp/celar-orchestrator.pid
