ab831911c3650d321b0044d17927fab8c19e05a6
Christos Mantas <cmantas@cslab.ece.ntua.gr>
2015-09-15 14:42:02 +0300

    available resources final

--- orchestrator-daemon/src/main/java/gr/ntua/cslab/orchestrator/rest/AvailableResources.java
+++ orchestrator-daemon/src/main/java/gr/ntua/cslab/orchestrator/rest/AvailableResources.java
@@ -1,10 +1,24 @@
+/*
+ * Copyright 2014 CELAR.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package gr.ntua.cslab.orchestrator.rest;
 
-import gr.ntua.cslab.celar.server.beans.ProvidedResource;
 import gr.ntua.cslab.celar.server.beans.structured.ProvidedResourceInfo;
 import gr.ntua.cslab.celar.server.beans.structured.REList;
 import static gr.ntua.cslab.database.EntitySearchers.searchProvidedResourceSpecs;
-import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -20,10 +34,9 @@ public class AvailableResources {
     
     @Path("flavors/")
     @GET
-    @Produces({MediaType.TEXT_PLAIN})
-    public static String getFlavors() throws Exception {
-        List<ProvidedResourceInfo> l= searchProvidedResourceSpecs("VM_FLAVOR");
-          return l.toString();
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public static REList<ProvidedResourceInfo> getFlavors() throws Exception {
+        return new REList(searchProvidedResourceSpecs("VM_FLAVOR"));
     }
     
     @Path("images/")
