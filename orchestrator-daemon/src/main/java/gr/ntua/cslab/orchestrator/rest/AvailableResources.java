ecccb25a297fef4c8cd925d7cc1f62ca8e4ccbb8
Christos Mantas <cmantas@cslab.ece.ntua.gr>
2015-09-15 13:55:19 +0300

    available resources TEST-5

--- orchestrator-daemon/src/main/java/gr/ntua/cslab/orchestrator/rest/AvailableResources.java
+++ orchestrator-daemon/src/main/java/gr/ntua/cslab/orchestrator/rest/AvailableResources.java
@@ -1,8 +1,10 @@
 package gr.ntua.cslab.orchestrator.rest;
 
+import gr.ntua.cslab.celar.server.beans.ProvidedResource;
 import gr.ntua.cslab.celar.server.beans.structured.ProvidedResourceInfo;
 import gr.ntua.cslab.celar.server.beans.structured.REList;
 import static gr.ntua.cslab.database.EntitySearchers.searchProvidedResourceSpecs;
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -18,9 +20,10 @@ public class AvailableResources {
     
     @Path("flavors/")
     @GET
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public static REList<ProvidedResourceInfo> getFlavors() throws Exception {
-          return new REList(searchProvidedResourceSpecs("VM_FLAVOR"));
+    @Produces({MediaType.TEXT_PLAIN})
+    public static String getFlavors() throws Exception {
+        List<ProvidedResourceInfo> l= searchProvidedResourceSpecs("VM_FLAVOR");
+          return l.toString();
     }
     
     @Path("images/")
